2fd24b7097dcedc747afabb08d3dadecca785080
neo99 <neo.l.h.wang@gmail.com>
2017-01-15 12:31:23 -0800

    return objects instead of strings

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Announcements.Models;
 
 namespace Announcements.Controllers
 {
@@ -10,9 +11,9 @@ namespace Announcements.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Announcement> Get()
         {
-            return new string[] {"new release on Feb 2, 2017", "emergency fix on Feb 1, 2017"};
+            return new Announcement[] {new Announcement("new release on Feb 2, 2017"), new Announcement("emergency fix on Feb 1, 2017")};
         }
     }
 }
