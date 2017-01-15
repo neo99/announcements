54c28d128c0b761077fd509471e99ab84dbc7cd3
neo99 <neo.l.h.wang@gmail.com>
2017-01-15 12:08:18 -0800

    add a simple api endpoint in HomeController

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace WebApplication1
+namespace Announcements
 {
     public class Program
     {
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
-namespace WebApplication1
+namespace Announcements
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Announcements!");
-            });
+            app.UseMvc();
         }
     }
 }
