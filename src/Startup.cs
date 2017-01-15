8d41ab29e8064b1f8c1a10181314b80873d08a25
Neo <neo.l.h.wang@gmail.com>
2017-01-14 21:37:21 -0800

    update greeting msg for testing

--- src/Startup.cs
+++ src/Startup.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Announcements!");
             });
         }
     }
